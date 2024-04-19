a684d07345e4f161316bfeff5af8ddb998b97f66
598459 <129184360+598459@users.noreply.github.com>
2024-04-19 15:06:25 -0300

    d

--- main.js
+++ main.js
@@ -16,36 +16,6 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-10T00:00:00");
-const tempoObjetivo4 = new Date("2024-12-10T00:00:00");
+let tempoAtual = nem Date();
 
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
-
-
-function calculaTempo(tempoObjetivo) {
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 1000);
-    let minutos = Math.floor(segundos / 60);
-    let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24);
-    
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
-    
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-}
-
-function atualizaCronometro(){
-    for (let i=0; i<contadores;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);
-    }
-}
-
-function comecaCronometro(){
-    atualizaCronometro();
-    setInterval(atualizaCronometro,1000);
-}
-comecaCronometro();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
