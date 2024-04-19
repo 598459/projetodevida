3c2cdd08fb6c6daa3b2e9249baf8dfcee2562c3f
598459 <129184360+598459@users.noreply.github.com>
2024-04-19 15:32:47 -0300

    x

--- main.js
+++ main.js
@@ -26,6 +26,11 @@ function calculaTempo(tempoObjetivo){
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24);
+
+    segundos %=60;
+    minutos
+    horas
+    dias
     return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
     
 }
