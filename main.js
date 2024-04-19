acbb6b6d0ab8deed18f942edbdcbd83aefb776d3
598459 <129184360+598459@users.noreply.github.com>
2024-04-19 15:52:22 -0300

    g

--- main.js
+++ main.js
@@ -22,8 +22,10 @@ const tempoObjetivo4 = new Date("2024-12-01T00:00:00");
 
 const tempo = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-contadores[0].textContent = calculaTempo(tempo[0]);
-contadores[1].textContent = calculaTempo(tempoObjetivo2);
+
+for(let i=0; i<contadores.length;i++){
+    contadores[i].textContent = calculaTempo(tempo[i]);
+}
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
