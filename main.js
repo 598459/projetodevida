0e8e156807a6f714582308c00995d563d70631b2
598459 <129184360+598459@users.noreply.github.com>
2024-04-19 15:15:42 -0300

    d

--- main.js
+++ main.js
@@ -1,28 +1,21 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
+for(let i=0; i <  botoes.length; i++){
+    botoes[i].onclick = function(){
 
-        for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.remove("ativo");
+        for(let j=0;j<botoes.length;j++){
+           botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
-        }
-
+ }    
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
+    
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+let tempoAtual = new Date();
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
-
-function calculaTempo(tempoObjetivo){
-    let tempoAtual = new Date();
-    let tempoFiinal = tempoObjetivo - tempoAtual;
-    let segundos = tempoFiinal1/1000;
-    return segundos; 
-}
-
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
