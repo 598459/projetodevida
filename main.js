f05a15b2c6714371fb37eac6f7b16910f03b6ad9
598459 <129184360+598459@users.noreply.github.com>
2024-04-19 15:54:50 -0300

    u

--- main.js
+++ main.js
@@ -22,9 +22,10 @@ const tempoObjetivo4 = new Date("2024-12-01T00:00:00");
 
 const tempo = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-
-for(let i=0; i<contadores.length;i++){
-    contadores[i].textContent = calculaTempo(tempo[i]);
+function atualizaCronometro(){
+    for(let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempo[i]);
+    }
 }
 
 function calculaTempo(tempoObjetivo){
